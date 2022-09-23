897e20b8b855226bd3bb06d454c5f95530793263
jaehyunlee <jaehyunlee25@gmail.com>
2022-09-23 16:30:30 +0900

    renew

---
+++ js/full.js
@@ -0,0 +1,18 @@
+let ANDROID_MAIN_UUID;
+let ac = false;
+try {
+  ac = window.AndroidBridge || window.webkit.messageHandlers.iosController;
+  ac.message =
+    ac.message || window.webkit.messageHandlers.iosController.postMessage;
+} catch (e) {
+  ac = false;
+}
+function setPopupMessage(str) {
+  console.log("str", str, typeof str);
+  const param = JSON.parse(str);
+  msgPopup.innerHTML = param.message;
+  console.log(param);
+  if (param.finish) {
+    if (ac) ac.message("REQUEST_END");
+  }
+}
