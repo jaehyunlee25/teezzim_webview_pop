eb02641d6a364eff03111131e6f3605995a33a5a
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-18 16:40:26 +0900

    renew

---
+++ pop/js/noti.js
@@ -0,0 +1,33 @@
+let ANDROID_MAIN_UUID;
+let ac = false;
+try {
+  ac = window.AndroidBridge || window.webkit.messageHandlers.iosController;
+  ac.message =
+    ac.message || window.webkit.messageHandlers.iosController.postMessage;
+} catch (e) {
+  ac = false;
+}
+function setAndroidMainUUID(uuid) {
+  ANDROID_MAIN_UUID = uuid;
+  /* elUUID.innerHTML = "UUID: " + ANDROID_MAIN_UUID; */
+}
+function setPopupMessage(str) {
+  console.log("str", str, typeof str);
+  const param = JSON.parse(str);
+  msgTitle.innerHTML = param.title;
+  msgPopup.innerHTML = param.message;
+  console.log(param);
+  /*
+    const param = {
+        total: 5,
+        success: 3,
+        fail: 1,
+        none: 1,
+        timeout: 1,
+        finish: false,
+    };
+    */
+  if (param.finish) {
+    if (ac) ac.message("REQUEST_END");
+  }
+}
