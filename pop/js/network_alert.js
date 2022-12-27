ecdb3f584ddd33303ceff380002c75e88ff02d71
jaehyunlee <jaehyunlee25@gmail.com>
2022-12-27 13:33:57 +0900

    'renew'

---
+++ pop/js/network_alert.js
@@ -0,0 +1,14 @@
+let ANDROID_MAIN_UUID;
+let ac = false;
+try {
+  ac = window.AndroidBridge || window.webkit.messageHandlers.iosController;
+  ac.message =
+    ac.message || window.webkit.messageHandlers.iosController.postMessage;
+} catch (e) {
+  ac = false;
+}
+function setAndroidMainUUID(uuid) {
+  ANDROID_MAIN_UUID = uuid;
+  /* elUUID.innerHTML = "UUID: " + ANDROID_MAIN_UUID; */
+}
+function setPopupMessage(str) {}
